13fcbda9a2cb35b0e78e564ff9c1be2826bce8d0
l0kr <okragly25@gmail.com>
2023-12-17 21:11:07 +0100

    whietspace emphasize not needed

--- modules/theme/theme.nix
+++ modules/theme/theme.nix
@@ -51,7 +51,6 @@ in
         " let g:monotone_color = [120, 100, 70] " Sets theme color to bright green
         let g:monotone_secondary_hue_offset = 320 " Offset secondary colors by 200 degrees
         let g:monotone_emphasize_comments = 1 " Emphasize comments
-        let g:monotone_emphasize_whitespace = 1 " Emphasize whitespace
       '';
 
       vim.startPlugins = with pkgs.neovimPlugins; (
